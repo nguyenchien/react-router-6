3344c81a232b905687aebe7cd3622589cb614f36
chienvn <chiennguyen1702@gmail.com>
2022-12-14 21:36:02 +0700

    NavLink style active

---
+++ src/component/StyledNavbar.js
@@ -0,0 +1,19 @@
+import {NavLink} from 'react-router-dom';
+
+const StyledNavbar = () => {
+  return (
+    <nav className='navbar'>
+      <NavLink to='/'
+      className={({isActive})=>isActive ? 'link active' : 'link'}
+      >home</NavLink>
+      <NavLink to='/about'
+      className={({isActive})=>isActive ? 'link active' : 'link'}
+      >about</NavLink>
+      <NavLink to='/products'
+      className={({isActive})=>isActive ? 'link active' : 'link'}
+      >products</NavLink>
+    </nav>
+  )
+}
+
+export default StyledNavbar
--- src/pages/ShareLayout.js
+++ src/pages/ShareLayout.js
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../component/Navbar';
+import StyledNavbar from '../component/StyledNavbar';
 
 const ShareLayout = () => {
   return (
     <>
-      <Navbar />
+      <StyledNavbar />
       <Outlet />
     </>
   );
