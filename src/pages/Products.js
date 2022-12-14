0651719edaf8880dacf61df2c35d073bbf22d8e8
chienvn <chiennguyen1702@gmail.com>
2022-12-15 06:16:40 +0700

    single product page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
+import products from '../data';
+
 const Products = () => {
   return (
     <>
       <section className='section'>
-        <h2>Products</h2>
+        <h2>products list</h2>
+        <ul>
+          {products.map((product)=> {
+            return (
+              <li key={product.id}>
+                <h3>{product.name}</h3>
+               <Link to={`/products/${product.id}`}>more detail</Link>
+              </li>
+            )
+          })}
+        </ul>
       </section>
     </>
   );
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,15 @@
 import { useParams, Link } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   const {productID} = useParams();
+  const product = products.find((product)=>product.id === productID);
+  const {name, image} = product;
   return (
     <section className='section product'>
-      <h2>{productID}</h2>
-      <Link to='/' className="btn">back to home</Link>
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <Link to='/products' className="btn">back to list</Link>
     </section>
   );
 };
