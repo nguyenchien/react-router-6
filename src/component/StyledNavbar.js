3ef24e2a3c7296c2409cf51995d4b577dffa55d4
chienvn <chiennguyen1702@gmail.com>
2022-12-14 21:52:30 +0700

    URL params

--- src/component/StyledNavbar.js
+++ src/component/StyledNavbar.js
@@ -6,9 +6,11 @@ const StyledNavbar = () => {
       <NavLink to='/'
       className={({isActive})=>isActive ? 'link active' : 'link'}
       >home</NavLink>
+      
       <NavLink to='/about'
       className={({isActive})=>isActive ? 'link active' : 'link'}
       >about</NavLink>
+      
       <NavLink to='/products'
       className={({isActive})=>isActive ? 'link active' : 'link'}
       >products</NavLink>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,11 @@
+import { useParams, Link } from "react-router-dom";
+
 const SingleProduct = () => {
+  const {productID} = useParams();
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>{productID}</h2>
+      <Link to='/' className="btn">back to home</Link>
     </section>
   );
 };
